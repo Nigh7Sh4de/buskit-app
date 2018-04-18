26239d2170b0f79aea307042e96261d07ef484fe
Dennis Plotnik <plotnikd@gmail.com>
2018-04-18 00:25:56 -0400

    Added log in button to authenticate with Twitch

--- App.js
+++ App.js
@@ -3,16 +3,40 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
+  Linking
 } from 'react-native';
 
-export default class App extends Component<{}> {
+var url = 'https://id.twitch.tv/oauth2/authorize?client_id=k6zpqqplgc8nyknrnkag6qhfpesc9p&redirect_uri=buskit://buskit.tv/redirect&response_type=token+id_token&scope=openid'
+
+export default class App extends Component {
+  componentDidMount() {
+    Linking.addEventListener('url', this.handleOpenURL);
+  }
+
+  handleOpenURL(event) {
+    console.log('Got intent-ed biatch', event);
+  }
+
+  login() {
+    console.log('lets do this')
+    Linking.openURL(url)
+    .catch(err => {
+      console.error(err)
+    })
+  }
+
   render() {
     return (
       <View>
         <Text>
           Yo
         </Text>
+        <Button
+          title="Go!"
+          onPress={this.login.bind(this)}
+          />
       </View>
     );
   }
