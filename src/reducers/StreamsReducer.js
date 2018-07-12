92eafb696ad2e2d0005515c74f95e3d764710d03
Dennis Plotnik <plotnikd@gmail.com>
2018-07-11 21:50:46 -0400

    Update reducer returns

--- src/reducers/StreamsReducer.js
+++ src/reducers/StreamsReducer.js
@@ -8,11 +8,16 @@ const initialState = {
 export default (state = initialState, action) => {
   switch(action.type) {
     case actions.STREAMS_FETCH_SUCCESS: 
-      return { ...state, data: action.streams, filtered_data: state.filtered_data || action.streams }
+      return { ...state, 
+        data: action.streams, 
+        filtered_data: state.filtered_data || action.streams 
+      }
     case actions.FILTER_UPDATED:
       const { filter } = action
       const filtered_data = state.data.filter(i => i.title.toLowerCase().indexOf(filter.text) >= 0)
-      return { ...state, filtered_data }
+      return { ...state, 
+        filtered_data,
+      }
     default: return state
   }
 }
--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -11,14 +11,12 @@ const initialState = {
 export default (state = initialState, action) => {
   switch(action.type) {
     case Actions.USER_LOGIN_SUCCESS:
-      return {
-        ...state,
+      return { ...state,
         user: action.user,
         loading: false
       }
     case Actions.USER_LOGIN_ERROR:
-      return {
-        ...state,
+      return { ...state,
         user: null,
         error: action.error,
         loading: false
@@ -26,27 +24,24 @@ export default (state = initialState, action) => {
     case Actions.USER_FETCH_DATA:
       const new_data = action.data.filter(u => !state.data.find(i => i.id === u.id))
       const data = [ ...state.data, ...new_data ]
-      return {
-        ...state,
+      return { ...state,
         data,
         filtered_data: state.filtered_data || data,
       }
     case Actions.FILTER_UPDATED:
       const { filter } = action  
       const filtered_data = state.data.filter(u => u.display_name.toLowerCase().indexOf(filter.text) >= 0)
-      return {
+      return { ...state,
         filtered_data,
       }
     case Actions.USER_LOGIN_LOADING:
-      return {
-        ...state,
+      return { ...state,
         user: null,
         error: null,
         loading: action.loading
       }
     case Actions.USER_LOGOUT:
-      return {
-        ...state,
+      return { ...state,
         user: null,
         loading: false
       }
